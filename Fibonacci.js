8973c9945c7308ba729a2b5c6c9b8995920000f5
Daniel Perez Atanacio <daniel.perez.atanacio@gmail.com>
2016-01-27 16:43:43 -0600

    Create Fibonacci.js

---
+++ Fibonacci.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let instance = Symbol();
+let instanceEnforcer = Symbol();
+
+/**
+ * Class Fibanacci Serie
+ */
+class Fibonacci{
+   
+
+    static get instance(){
+        if (!this[instance]) {
+            this[instance] = new Fibonacci(instanceEnforcer);
+        }
+
+        return this[instance];
+    }
+
+    constructor(enforcer){
+        if (enforcer !== instanceEnforcer) throw ('Cannot construct singleton');
+    }
+
+    execute(number){
+        if (number == 0 || number == 1) return number;
+
+        return this.execute(number-2) + this.execute(number-1);
+    }
+
+    static print(){
+        let res = '';
+        let fibo = Fibonacci.instance;
+
+        for (let i = 0; i < 10; i++){
+            res += `Fibonacci(${i}) = ${fibo.execute(i)} \n`;
+        }
+
+        return res;
+    }
+}
